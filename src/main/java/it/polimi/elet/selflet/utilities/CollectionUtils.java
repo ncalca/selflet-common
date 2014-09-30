55760e4ee70c98c1df69e6776dc5dda237d3aae0
elleFlorio <elle.florio@gmail.com>
2014-09-30 12:05:49 +0200

    Bug fix - synchronization of iterable

--- src/main/java/it/polimi/elet/selflet/utilities/CollectionUtils.java
+++ src/main/java/it/polimi/elet/selflet/utilities/CollectionUtils.java
@@ -2,6 +2,7 @@ package it.polimi.elet.selflet.utilities;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import com.google.common.collect.Lists;
@@ -22,7 +23,8 @@ public class CollectionUtils {
 		return list.get(0);
 	}
 
-	public static <T extends IWeightedItem> T weightedRandomElement(Collection<T> collection) {
+	public static <T extends IWeightedItem> T weightedRandomElement(
+			Collection<T> collection) {
 
 		checkEmpty(collection);
 		checkWeights(collection);
@@ -43,7 +45,9 @@ public class CollectionUtils {
 			}
 		}
 
-		throw new IllegalStateException("Cannote extract random item from collection. Total weight: " + totalWeight);
+		throw new IllegalStateException(
+				"Cannote extract random item from collection. Total weight: "
+						+ totalWeight);
 	}
 
 	public static <T> void checkEmpty(final Collection<T> collection) {
@@ -53,14 +57,17 @@ public class CollectionUtils {
 		}
 
 		if (collection.isEmpty()) {
-			throw new IllegalArgumentException("Trying to extract a random element from an empty collection");
+			throw new IllegalArgumentException(
+					"Trying to extract a random element from an empty collection");
 		}
 	}
 
-	public static <T extends IWeightedItem> void checkWeights(final Collection<T> collection) {
+	public static <T extends IWeightedItem> void checkWeights(
+			final Collection<T> collection) {
 		for (T item : collection) {
 			if (item.weight() < 0) {
-				throw new IllegalArgumentException("Weight of item cannot be zero or negative: " + item);
+				throw new IllegalArgumentException(
+						"Weight of item cannot be zero or negative: " + item);
 			}
 		}
 	}
@@ -69,16 +76,19 @@ public class CollectionUtils {
 	 * Returns 0 if empty iterable
 	 * */
 	public static double computeAverage(Iterable<Double> iterable) {
-		int numberOfElements = 0;
-		double sum = 0;
-		for (Double value : iterable) {
-			sum += value;
-			numberOfElements++;
+		synchronized (iterable) {
+			Iterator<Double> iterator = iterable.iterator();
+			int numberOfElements = 0;
+			double sum = 0;
+			while (iterator.hasNext()) {
+				sum += iterator.next();
+				numberOfElements++;
+			}
+			if (numberOfElements == 0) {
+				return 0;
+			}
+			return sum / numberOfElements;
 		}
-		if (numberOfElements == 0) {
-			return 0;
-		}
-		return sum / numberOfElements;
 	}
 
 }
